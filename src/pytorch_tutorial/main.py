b35cb2d2de08edf1d3d1facfb0420090d7b0b0c9
dyoshyy <d.yoshhyy@gmail.com>
2024-06-17 19:39:31 +0900

    feat: mainをCLI形式に

--- src/pytorch_tutorial/main.py
+++ src/pytorch_tutorial/main.py
@@ -1,7 +1,24 @@
-# from pytorch_tutorial.GAN import GenerativeAdversarialNetworks
+import argparse
+
+from pytorch_tutorial.GAN import GenerativeAdversarialNetworks
 from pytorch_tutorial.VAE import VariationalAutoencoder
 
 
 def main() -> None:
-    # GenerativeAdversarialNetworks()
-    VariationalAutoencoder()
+    parser = argparse.ArgumentParser(description="Run a specific model.")
+    parser.add_argument(
+        "model",
+        type=str,
+        choices=["GAN", "VAE"],
+        help="The model to run: 'GAN' or 'VAE'",
+    )
+    args = parser.parse_args()
+
+    model = args.model
+
+    if model == "GAN":
+        GenerativeAdversarialNetworks()
+    elif model == "VAE":
+        VariationalAutoencoder()
+    else:
+        raise ValueError(f"Unknown model: {model}")
